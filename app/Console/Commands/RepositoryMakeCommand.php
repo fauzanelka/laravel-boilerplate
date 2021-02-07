f5093575b98f1625885afabf0c21d6d83ccdc435
Fauzan Elka <fauzan.elka@hotmail.com>
2021-02-07 21:08:28 +0700

    Update make:repository command

--- app/Console/Commands/RepositoryMakeCommand.php
+++ app/Console/Commands/RepositoryMakeCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Illuminate\Support\Str;
 
 class RepositoryMakeCommand extends GeneratorCommand
 {
@@ -38,13 +40,76 @@ class RepositoryMakeCommand extends GeneratorCommand
     }
 
     /**
-     * Get the default namespace for the class.
+     * Get the destination class path.
      *
-     * @param  string  $rootNamespace
+     * @param  string  $name
      * @return string
      */
-    protected function getDefaultNamespace($rootNamespace)
+    protected function getPath($name)
     {
-        return $rootNamespace . '\Repositories';
+        $name = (string) Str::of($name)->replaceFirst($this->rootNamespace(), '')->finish('Repository');
+
+        return app_path().'/Repositories/'.str_replace('\\', '/', $name).'.php';
+    }
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param string $name
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     * @return string
+     */
+    protected function buildClass($name)
+    {
+        $repository = class_basename(Str::ucfirst(str_replace('Repository', '', $name)));
+
+        $namespaceModel = $this->option('model')
+                        ? $this->qualifyModel($this->option('model'))
+                        : $this->guessModelName($name);
+
+        $replace = [
+            '{{ repositoryNamespace }}' => 'App\\Repositories',
+            '{{ namespacedModel }}' => $namespaceModel,
+            '{{namespacedModel}}' => $namespaceModel,
+            '{{ repository }}' => $repository,
+            '{{repository}}' => $repository,
+        ];
+
+        return str_replace(
+            array_keys($replace), array_values($replace), parent::buildClass($name)
+        );
+    }
+
+    /**
+     * Guess the model name from the Repository name or return a default model name.
+     *
+     * @param string $name
+     * @return string
+     */
+    protected function guessModelName($name)
+    {
+        if (Str::endsWith($name, 'Repository')) {
+            $name = substr($name, 0, -10);
+        }
+
+        $modelName = $this->qualifyModel(Str::after($name, $this->rootNamespace()));
+
+        if (class_exists($modelName)) {
+            return $modelName;
+        }
+
+        return 'Illuminate\Database\Eloquent\Model';
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['model', 'm', InputOption::VALUE_OPTIONAL, 'The name of the model']
+        ];
     }
 }
